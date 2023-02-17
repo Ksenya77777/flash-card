4de9b7315eef45e1e4791a55f07d73e0c13b7b8b
Kseniya Smaliakova <kseniya-smaliakova1993@yandex.ru>
2023-02-17 11:48:15 +0300

    fs.readFileSync

--- Model.js
+++ Model.js
@@ -1,7 +1,26 @@
-class Model {
-  constructor() {
+const fs = require("fs");
+const { EOL } = require("os");
 
+class Model {
+  constructor(topic) {
+    this.topic = topic;
   }
+
+  readAsk() {
+    const ask = fs.readFileSync(
+      `${__dirname}/topics/${this.topic}.txt`,
+      "utf-8"
+    );
+		const arrAsk = ask.split(EOL);
+		const arrQustion = arrAsk.filter((el,index) => index % 2 === 1);
+		return arrQustion;
+  }
+	readAnswers (){
+		const answer = fs.readFileSync(`${__dirname}/topics/${this.topic}.txt`, 'utf-8');
+		const arrAnswer = answer.split(EOL);
+		const arrAns = arrAnswer.filter((el,index) => index % 2 === 0);
+		return arrAns;
+	}
 }
 
-module.exports = Model
+module.exports = Model;
